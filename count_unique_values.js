f9548cccde3ba0335ff6eca3473475f97fe63d71
NickSpangler <Spangleyman@mac.com>
2021-02-02 17:07:00 -0500

    better solution

--- count_unique_values.js
+++ count_unique_values.js
@@ -2,21 +2,35 @@
 // and counts the unique values in the array. There can be negative numbers in 
 // the array, but it will always be sorted
 
+// function countUniqueValues(arr) {
+    // if (arr.length === 0) return 0
+//     let left = 0;
+//     let right = 1;
+//     while (right < arr.length) {
+//         if (arr[left] === arr[right]) {
+//             right++
+//         } else {
+//             left++
+//             arr[left] = arr[right]
+//             right++
+//         }
+//     }
+//     console.log(left + 1)
+//     return left + 1
+// }
+
+
+//BETTER SOLUTION
 function countUniqueValues(arr) {
     if (arr.length === 0) return 0
-    let left = 0;
-    let right = 1;
-    while (right < arr.length) {
-        if (arr[left] === arr[right]) {
-            right++
-        } else {
-            left++
-            arr[left] = arr[right]
-            right++
+    let i = 0;
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++
+            arr[i] = arr[j]
         }
     }
-    console.log(left + 1)
-    return left + 1
+    return i + 1;
 }
 
 countUniqueValues([1,1,1,1,1,1,2])
