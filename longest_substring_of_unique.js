2d5a905cc77e11e705a627c1541ec1b3985ca00e
NickSpangler <Spangleyman@mac.com>
2021-02-03 12:17:19 -0500

    begin solving

--- longest_substring_of_unique.js
+++ longest_substring_of_unique.js
@@ -2,5 +2,14 @@
 //preferably using a sliding window.
 
 function uniqueSubstring(string) {
-    
+    if (string.length === 0) return null;
+    let maxSub = 1
+    let tempSub = 1
+    let subString = string[0]
+    for (let i = 1; i < string.length; i++) {
+
+    }
+    return maxSub;
 }
+
+"hello"
