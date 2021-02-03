60d2ef4a98dbc467bf5fc71d98f99f37c1defffb
NickSpangler <Spangleyman@mac.com>
2021-02-03 12:56:25 -0500

    add notes on refactor

--- longest_substring_of_unique.js
+++ longest_substring_of_unique.js
@@ -21,13 +21,19 @@
 
 function uniqueSubstring(string) {
     let left = 0, right = 0, max = 0, charSet = new Set()
+    //this has multiple pointers, a max, and a set object, of only unique values
     while (right < string.length) {
         console.log(charSet)
+        //start BOTH pointers at 0, so the first character gets added to the Set
         if (!charSet.has(string.charAt(right))) {
+            //set is empty, so char gets added
             charSet.add(string.charAt(right))
             right++
+            //always update/compare max when you've expanded the window
             max = Math.max(charSet.size, max)
         } else {
+            //this closes the window from the left, deleting each element as it
+            //goes, until it has finally deleted the repeated char we have just encountered
             charSet.delete(string.charAt(left))
             left++
         }
