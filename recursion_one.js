9c7ef20e51f08682d1a624508a8ad4eb7cae5c1d
NickSpangler <Spangleyman@mac.com>
2021-02-03 16:52:26 -0500

    define countDown()

--- recursion_one.js
+++ recursion_one.js
@@ -1,2 +1,17 @@
  //A simple example of recursion
- 
+
+ function countDown(num) {
+     //ESTABLISH YOUR BASE CASE AND WHAT SHOULD HAPPEN WHEN IT IS HIT
+     if (num <= 0) {
+         console.log('All done!')
+         return
+     }
+     //THIS IS WHAT EACH ITERATION WILL DO
+     console.log(num);
+
+     //HERE IS WHERE YOU CHANGE THE INPUT AND CALL THE FUNCTION AGAIN
+     num--
+     countDown(num)
+ }
+
+ countDown(10)
