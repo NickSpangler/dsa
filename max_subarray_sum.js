235504158c1f006fe24a2d9aefa9d486ffcbb1a4
NickSpangler <Spangleyman@mac.com>
2021-02-03 14:59:35 -0500

    define problem

---
+++ max_subarray_sum.js
@@ -0,0 +1,7 @@
+//Given an array of integers and a number, write a function called maxSubarraySum which finds the maximum sum
+//of a subarray with the length of the number passed in to the function
+
+function maxSubarraySum(array, num) {
+
+}
+
