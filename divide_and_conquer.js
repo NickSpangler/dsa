b03e477703d4f7db7a8c9db40963e207948a9e97
NickSpangler <Spangleyman@mac.com>
2021-02-03 13:11:07 -0500

    write out Binary Search

--- divide_and_conquer.js
+++ divide_and_conquer.js
@@ -3,3 +3,23 @@
 //a process with a subset of data
 
 //can tremendously decrease time complexity
+
+//BINARY SEARCH is an example of this, as seen below
+
+function binarySearch(array, value) {
+    let left = 0;
+    let right = array.length - 1
+    while (left < right) {
+        let mid = Math.floor((min + max) / 2)
+        let currentElement = array[mid]
+
+        if (array[mid] < value) {
+            left = mid + 1
+        } else if (array[mid] > value) {
+            right = mid - 1
+        } else {
+            return middle;
+        }
+    }
+    return -1
+}
