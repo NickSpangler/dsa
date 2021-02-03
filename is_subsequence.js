24140f4d6d3cabe1346a067f75f3581e3e6c5f8a
NickSpangler <Spangleyman@mac.com>
2021-02-03 14:32:23 -0500

    create is_subsequence

---
+++ is_subsequence.js
@@ -0,0 +1,6 @@
+//write a function called isSubsequence which takes in two strings and checks
+//whether the charactrs in the first string appear somewhere in the second string
+//without their order changing
+
+//USE MULTIPLE POINTERS
+
