3388f62e952793124c0de23a337867980bdb485c
NickSpangler <Spangleyman@mac.com>
2021-02-03 12:12:38 -0500

    define uniqueSubstring() function

---
+++ longest_substring_of_unique.js
@@ -0,0 +1,6 @@
+//Given a string, find the longest substring of unique characters
+//preferably using a sliding window.
+
+function uniqueSubstring(string) {
+    
+}
--- sliding_window.js
+++ sliding_window.js
@@ -25,3 +25,4 @@ function maxSubarraySum(arr, num) {
     }
     return maxSum
 }
+
