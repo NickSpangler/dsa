2abd8396a3bce332c1983d7aaf26e1598f3f1b2d
NickSpangler <Spangleyman@mac.com>
2021-02-03 13:50:41 -0500

    create average_pair.js

---
+++ average_pair.js
@@ -0,0 +1,6 @@
+//write a function called averagePair, which takes a SORTED ARRAY of integers and a
+//target average. Determine if there is a pair of values in the array where
+//the average of the pair equals the target average. 
+
+//There may be more than one pair that matches the average target.
+
