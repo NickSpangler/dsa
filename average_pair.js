4901e3883b8e5bcfc2d711e7d8035a3e25fefe50
NickSpangler <Spangleyman@mac.com>
2021-02-03 14:30:45 -0500

    correct!

--- average_pair.js
+++ average_pair.js
@@ -4,3 +4,31 @@
 
 //There may be more than one pair that matches the average target.
 
+function averagePair(arr, n) {
+    //define two pointers on outside of arr
+    //average the pair
+    //if average equals n, return true
+    //if average is greater than n, decrement right
+    //if average is less than n, increment left
+    if (arr.length < 2) return false
+    let left = 0
+    let right = arr.length - 1
+    while (left < right) {
+        let average = (arr[left] + arr[right] / 2)
+        if (average === n) {
+            console.log('true')
+            return true
+        } else if (average > n) {
+            right--
+        } else {
+            left++
+        }
+    }
+    console.log('false')
+    return false
+}
+
+averagePair([1,2,3], 2.5)
+averagePair([1,2,2,5,6,7,10,12, 19], 8)
+averagePair([-1,0,3,4,5,6], 4.1)
+averagePair([],4)
