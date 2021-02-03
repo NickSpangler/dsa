4bef39420dee176c986e4e264aabb70fe19158b7
NickSpangler <Spangleyman@mac.com>
2021-02-03 14:47:58 -0500

    clarify instructions

--- is_subsequence.js
+++ is_subsequence.js
@@ -2,13 +2,16 @@
 //whether the charactrs in the first string appear somewhere in the second string
 //without their order changing
 
+//THE SUBSTRING DOES NOT HAVE TO APPEAR CONSECUTIVELY
+
 //USE MULTIPLE POINTERS
 
 function isSubsequence(sub, string) {
     if (sub.length > string.length) return false
     let subPointer = 0, stringPointer = 0
     while (stringPointer < string.length) {
-        if (sub[subPointer === string[stringPointer]]) {
+        console.log(subPointer)
+        if (sub[subPointer] === string[stringPointer]) {
             subPointer++
             stringPointer++
             if (subPointer === sub.length - 1) {
@@ -24,6 +27,11 @@ function isSubsequence(sub, string) {
     console.log('false')
     return false
 }
+
+isSubsequence('hello', 'hello world')
+isSubsequence('sing', 'sting')
+isSubsequence('abc', 'abracadabra')
+isSubsequence('abc', 'acb')
 
 
 //create a pointer starting at zero for sub
