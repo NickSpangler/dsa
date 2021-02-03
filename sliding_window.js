03b148a990dc31d100157a51ebfb6493bf10f0f5
NickSpangler <Spangleyman@mac.com>
2021-02-03 11:07:39 -0500

    describe what a sliding window is

---
+++ sliding_window.js
@@ -0,0 +1,8 @@
+//A pattern involving creating a window, which can be either an array or a number
+//from one position to another
+
+//Depending on a condition, the window either increases or closes, (and then a new
+//window is created)
+
+//Useful for keeping traack of a subset of data in an array or string.
+
