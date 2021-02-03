58cb009c0c42dfbfe092d5f639b213cd95bd77c7
NickSpangler <Spangleyman@mac.com>
2021-02-03 15:16:46 -0500

    correct!

--- max_subarray_sum.js
+++ max_subarray_sum.js
@@ -2,6 +2,33 @@
 //of a subarray with the length of the number passed in to the function
 
 function maxSubarraySum(array, num) {
+    if (array.length < num) return null;
+    let max = -Infinity
+    let currentSum = 0
+    for (let i = 0; i < num; i++) {
+        currentSum += array[i]
+    }
+    console.log(currentSum)
+    max = Math.max(max, currentSum)
 
+    for (let i = num; i < array.length; i++) {
+        console.log(array[i - num], array[i])
+        currentSum = (currentSum - array[i - num]) + array[i]
+        console.log(currentSum)
+        max = Math.max(max, currentSum)
+    }
+    console.log(max)
+    return max
 }
 
+// maxSubarraySum([100, 200, 300, 400], 2)
+maxSubarraySum([1,4,2,10,23,3,1,0,20], 4)
+// maxSubarraySum([-3,4,0,-2,6,-1], 2)
+// maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1], 2)
+// maxSubarraySum([2,3], 3)
+//define max as -Infinity
+//sum first num elements, compare to max
+//begin iteration at num + 1
+//move forward, subtracting first element in the window and adding new element
+//continue to compare current sum to max
+//return max sum
