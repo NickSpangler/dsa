23198665132546ad27023591835d1e66622b163d
NickSpangler <Spangleyman@mac.com>
2021-02-03 12:41:02 -0500

    second attempt

--- longest_substring_of_unique.js
+++ longest_substring_of_unique.js
@@ -1,18 +1,22 @@
 //Given a string, find the longest substring of unique characters
 //preferably using a sliding window.
 
-function uniqueSubstring(string) {
-    if (string.length === 0) return null;
-    let maxSub = 1
-    let left = 0
-    let right = 1
-    for (let i = 1; i < string.length; i++) {
-        if (string.slice(left, i).contains(string[i])) {
-            maxSub = Math.max(maxSub, string.slice(left, i))
-            left++
-        }
-    }
-    return maxSub;
-}
+// function uniqueSubstring(string) {
+//     if (string.length === 0) return null;
+//     let maxSub = 1
+//     let left = 0
+//     let right = 1
+//     for (let i = 1; i <= string.length; i++) {
+//         console.log(string.slice(left, i))
+//         if (string.slice(left, i).includes(string[i])) {
+//             maxSub = Math.max(maxSub, string.slice(left, i).length)
+//             left = string.slice(left, i).indexOf(string[i]) + 1
+//         } else {
+//             maxSub = Math.max(maxSub, string.slice(left, i).length)
+//         }
+//     }
+//     console.log(maxSub)
+//     return maxSub;
+// }
 
-"hello"
+uniqueSubstring('tnenent')
