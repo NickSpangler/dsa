695357aae5d698a8a507a51a7d68d933bf0f9da4
NickSpangler <Spangleyman@mac.com>
2021-02-03 16:23:41 -0500

    first try! Clean solution.

--- find_longest_substring.js
+++ find_longest_substring.js
@@ -2,5 +2,20 @@
 //of the longest substring with all distinct characters
 
 function findLongestSubstring(string) {
-    
+    if (string.length === 0) return 0
+    let set = new Set()
+    let max = 0;
+    let left = 0;
+    let right = 0;
+    while (right < string.length) {
+        if (!set.has(string[right])) {
+            set.add(string[right])
+            max = Math.max(max, set.size)
+            right++
+        } else {
+            set.delete(string[left])
+            left++
+        }
+    }
+    return max;
 }
