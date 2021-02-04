e05c19b3e7f7ae0147856ae7eb626752b007a9f2
NickSpangler <Spangleyman@mac.com>
2021-02-04 14:18:25 -0500

    Working solution

--- recursion_power.js
+++ recursion_power.js
@@ -2,3 +2,26 @@
 //Should return the power of the base to the exponent
 //This should mimic Math.pow() - do not worry about negative bases and exponents
 
+function power(base, exponent) {
+    let total = base
+    console.log(`Total = ${total}`)
+    function recursiveHelper(exp) {
+        if (exp === 0) return total * 1;
+        console.log(`Multiplying ${total} by helper ${exp}`)
+        return total *= recursiveHelper(exp - 1)
+    }
+    recursiveHelper(exponent - 1)
+    console.log(total)
+    return total;
+}
+
+//define total = 1
+//define recursive function that takes in a power
+//multiple base by itself until the power = 0
+//BASE CASE IS WHEN POWER = 0
+//total will accrue the value
+//return the total
+
+power(2, 3)
+power(3, 2)
+power(5, 5)
