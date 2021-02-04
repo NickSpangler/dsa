235a5c680cd802ba25dd9cbf1cf376a3697ebeca
NickSpangler <Spangleyman@mac.com>
2021-02-04 12:03:00 -0500

    Outline helper method recursion pattern

---
+++ helper_method_recursion.js
@@ -0,0 +1,11 @@
+// A pattern with an OUTER FUNCTION
+//and inside there is an INNER FUNCTION that itself is recursive
+
+//Commonly used when COLLECTING a set, or building an array
+
+//1) Define outter function
+//2) Define collection; let results = []
+//3) Define inner/recursive funciton, which updates results
+//4) CALL inner function with original input
+//5) Return results
+
