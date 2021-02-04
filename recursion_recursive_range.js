c7ca515a5fbc3dd4e181b6479de60cf66cc54c35
NickSpangler <Spangleyman@mac.com>
2021-02-04 14:29:41 -0500

    correct!

---
+++ recursion_recursive_range.js
@@ -0,0 +1,6 @@
+//write recursiveRange, which accepts a number and adds up all the numbers from 0 to that number
+
+function recursiveRange(num) {
+    if (num === 0) return 0;
+    return num + recursiveRange(num - 1)
+}
