35a32bc99b3b81ed91de1d229ddc9557d312b321
NickSpangler <Spangleyman@mac.com>
2021-02-04 12:12:50 -0500

    add notes

--- helper_method_recursion.js
+++ helper_method_recursion.js
@@ -10,14 +10,16 @@
 //5) Return results
 
 function collectOddNumbers(arr) {
-    let odds = []
-    function getOdds(array) {
-        if (array.length === 0) return
-        if (array[0] % 2 !== 0) {
-            odds.push(array[0])
+    let odds = [] //define collector
+    function getOdds(array) { //define helper method
+        if (array.length === 0) return //BASE CASE, when array is empty
+        if (array[0] % 2 !== 0) {//Test if first element is odd
+            odds.push(array[0])//if odd, push to collector
         }
-        getOdds(array.slice(1))
+        getOdds(array.slice(1))//call recursively, minus the first element, regardless
     }
-    getOdds(arr)
-    return odds;
+    getOdds(arr)//call helper function
+    return odds;//return collector
 }
+
+//NOTE: base case returns, and then OUTER FUNCTION RETURNS
