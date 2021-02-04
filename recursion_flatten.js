2c4a1cbd2160860e4a64a53f1555057a9c45c72e
NickSpangler <Spangleyman@mac.com>
2021-02-04 16:05:02 -0500

    correct!

--- recursion_flatten.js
+++ recursion_flatten.js
@@ -1,19 +1,35 @@
 //write flatten, which takes an array of arrays and returns a new array with all values flattened
 
+//COULD NOT SOLVE
+
 function flatten(arr) {
-    let flattened = []
-    function recursion(arr) {
-        if (arr.length === 0) return
-        if (typeof(arr[0]) !== "object") {
-            flattened.push(arr[0])
-            return recursion(arr.slice(1))
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            newArr = newArr.concat(flatten(arr[i]))
         } else {
-            return recursion(arr[0])
+            newArr.push(arr[i])
         }
     }
-    return flattened
+    return newArr;
 }
+// function flatten(arr) {
+//     let flattened = []
+//     function recursion(arr) {
+//         if (arr.length === 0) return
+//         if (typeof(arr[0]) === "number") {
+//             console.log('I am not an array!')
+//             flattened.push(arr[0])
+//             return recursion(arr.slice(1))
+//         } else {
+//             return recursion(arr[0])
+//         }
+//     }
+//     recursion(arr)
+//     return flattened
+// }
 
+// flatten([1, 2, 3, [4, 5] ])
 //define empty array
 //define recursion
 //look at first element, if it is not an array, push it to new array
