4e3259a666046827fde6ffc329786dd3ce35db64
NickSpangler <Spangleyman@mac.com>
2021-02-04 14:55:05 -0500

    correct!

--- recursion_reverse.js
+++ recursion_reverse.js
@@ -1,2 +1,12 @@
 //Write a recursive function called reverse which accepts a string and returns a new string in reverse
 
+function reverse(string) {
+    if (string.length === 0) {
+        console.log('END')
+        return ''
+    }
+    return string[string.length - 1] + reverse(string.slice(0, string.length - 1))
+}
+
+reverse('hello')
+// string[string.length - 1] + reverse(string.slice(0, string.length - 1))
