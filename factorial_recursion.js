869825812e23cfe8dfc718992b7c1bd7832216ad
NickSpangler <Spangleyman@mac.com>
2021-02-04 11:49:42 -0500

    wrong recursive Factorial

--- factorial_recursion.js
+++ factorial_recursion.js
@@ -2,7 +2,7 @@
 
 function iterativeFactorial(num) {
     let total = 1
-    for (let i = num; i > 1; i++) {
+    for (let i = num; i > 1; i--) {
         total *= i
     }
     console.log(total)
@@ -11,3 +11,14 @@ function iterativeFactorial(num) {
 iterativeFactorial(3)
 iterativeFactorial(4)
 iterativeFactorial(5)
+
+function recursiveFactorial(num) {
+    if (num === 1) return 1;
+    let total = num
+    total *= recursiveFactorial(num - 1)
+    console.log(total)
+}
+
+recursiveFactorial(3)
+recursiveFactorial(4)
+recursiveFactorial(5)
