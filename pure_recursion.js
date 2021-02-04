fa44f692912cc382d19562fdb387104434e8e320
NickSpangler <Spangleyman@mac.com>
2021-02-04 12:19:55 -0500

    first attempt

---
+++ pure_recursion.js
@@ -0,0 +1,11 @@
+//
+
+function collectOdds(arr, odds = []) {
+    if (arr.length === 0) return
+    if (arr[0] % 2 !== 0) {
+        odds.push(arr[0])
+    }
+    collectOdds(arr.slice(1), odds)
+    console.log(odds)
+    return odds
+}
