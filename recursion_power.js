abc368ad8add9d3b7b60ce09419dcc1e068d9b03
NickSpangler <Spangleyman@mac.com>
2021-02-04 14:06:14 -0500

    define problem

---
+++ recursion_power.js
@@ -0,0 +1,4 @@
+//Write a function called power which accepts a base and an exponent.
+//Should return the power of the base to the exponent
+//This should mimic Math.pow() - do not worry about negative bases and exponents
+
