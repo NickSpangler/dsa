9b9fa5ebe8989f6d63fee1cf16b973b8f5172e32
NickSpangler <Spangleyman@mac.com>
2021-02-05 11:46:10 -0500

    refactor for concise code

--- search/binary_search.js
+++ search/binary_search.js
@@ -11,16 +11,11 @@ function binarySearch(arr, target) {
     let right = arr.length - 1
     let mid = Math.floor((right + left) / 2)
     while (arr[mid] !== target && left <= right) {
-        if (arr[mid] > target) {
-            right = mid - 1
-        } else {
-            left = mid + 1
-            
-        }
+        if (arr[mid] > target) right = mid - 1
+        else left = mid + 1
         mid = Math.floor((right + left) / 2)
     }
-    if (arr[mid] === target) return mid
-    return - 1
+    return arr[mid] === target ? mid : - 1
 }
 
 binarySearch([1,2,3,4,5], 2)
