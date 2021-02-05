de5b64f9f9e2c59d46b115f37f47513cf15f7057
NickSpangler <Spangleyman@mac.com>
2021-02-05 10:47:28 -0500

    correct!

--- recursion_nested_even_sum.js
+++ recursion_nested_even_sum.js
@@ -1,5 +1,32 @@
 //write nestedEvenSum(), which returns the sum of all even numbers in an object, which may contain nested objects
 
+//Solved using Helper Method Recursion
 function nestedEvenSum(obj) {
+    let total = 0
 
+    function addEvens(obj) {
+        for (let key in obj) {
+            if (typeof obj[key] === 'number' && obj[key] % 2 === 0) {
+                total += obj[key]
+            } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+                addEvens(obj[key])
+            }
+        }
+    }
+    addEvens(obj)
+    return total
 }
+
+var obj1 = {
+    outer: 2,
+    obj: {
+      inner: 2,
+      otherObj: {
+        superInner: 2,
+        notANumber: true,
+        alsoNotANumber: "yup"
+      }
+    }
+  }
+
+  nestedEvenSum(obj1)
