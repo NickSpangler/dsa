0baa88a0187561a9825e36a9f8523819be09057b
NickSpangler <Spangleyman@mac.com>
2021-02-05 11:18:39 -0500

    halfway done

--- search/binary_search.js
+++ search/binary_search.js
@@ -5,3 +5,15 @@
     //if mid is too smalle, move left up
     //if mid is too big, move right down
 //if you never find the value, return - 1
+
+function binarySearch(arr, target) {
+    let left = 0
+    let right = arr.length - 1
+    while (left < right) {
+        let mid = Math.floor((right - left) / 2)
+        if (arr[mid] === target) return mid;
+        if (arr[mid] > target) {
+            right = mid - 1
+        }
+    }
+}
