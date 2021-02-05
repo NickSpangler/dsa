6b237f67d1fb6ad3a748a90e39ded2fa632f36b5
NickSpangler <Spangleyman@mac.com>
2021-02-05 13:44:50 -0500

    numberCompare example

--- sorting/JS_sort.js
+++ sorting/JS_sort.js
@@ -1,1 +1,13 @@
 //How does .sort() work in JS?
+
+//it can take a comparator function, which takes (a, b)
+//if the function returns a negative number, a comes first
+//if the function returns a positive number, b comes first
+//if the function returns 0, a and b are equal
+
+//this will return negative, positive, or 0, and .sort() sorts on this criteria
+function numberCompare(num1, num2) {
+    return num1 - num2
+}
+
+console.log([500, -52, 49, 2, 17, 8000].sort(numberCompare))
