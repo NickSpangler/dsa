ca2b9e8258854b83a021ce89348d981fb4cba934
NickSpangler <Spangleyman@mac.com>
2021-02-05 11:16:03 -0500

    create notes for binary_search.js

---
+++ search/binary_search.js
@@ -0,0 +1,7 @@
+//binarySearch accepts a sorted array and a value
+//create left point at start and right pointer at end
+//while left < right, pick a middle
+    //check if mid === target
+    //if mid is too smalle, move left up
+    //if mid is too big, move right down
+//if you never find the value, return - 1
