688db9745a03c4fe19c803498375ded1a6508a42
NickSpangler <Spangleyman@mac.com>
2021-02-05 14:20:16 -0500

    optimize, checking for swaps

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -8,12 +8,17 @@
 //start an inner loop called j, from the beginning until i  -1
 //if arr[i] is greater than arr[j+1], swap
 
+//optimized with variable noSwaps
 function bubbleSort(array) {
+    let noSwaps;
     let arr = array.slice(0)
     for (let i = arr.length; i > 0; i--){
+        noSwaps = true;//in outter loop, noSwaps always becomes true
         for (let j = 0; j < i - 1; j++){
             if (arr[j] > arr[j+1]) swap(arr, j)
+            noSwaps = false //noSwaps is set to false, after our first swap
         }
+        if (noSwaps) break;//outter loop checks if noSwaps is true/false
     }
     return arr;
 }
