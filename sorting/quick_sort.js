63edca21f285b1a0f0302279841fcb63cc84494a
NickSpangler <Spangleyman@mac.com>
2021-02-09 12:41:36 -0500

    implement pivotHelper()

--- sorting/quick_sort.js
+++ sorting/quick_sort.js
@@ -10,4 +10,19 @@
     // puts it at the beginning, and puts pivot directly after all elements smaller than itself)
     //RETURN the pivot index
 
-    
+    function pivotHelper(arr, start = 0, end = arr.length - 1) {
+        let pivot = arr[start]
+        let pivotCounter = 0
+        for (let i = 0; i < arr.length; i++) {
+            if (pivot > arr[i]) {
+                pivotCounter++
+                [arr[i], arr[pivotCounter]] = [arr[pivotCounter], arr[i]]
+            }
+        }
+        [arr[start], arr[pivotCounter]] = [arr[pivotCounter], arr[start]]
+        console.log(arr)
+        console.log(`Counter: ${pivotCounter}`)
+        return pivotCounter
+    }
+
+    pivotHelper([69, 1, 72, 55, 90, 26, 13])
