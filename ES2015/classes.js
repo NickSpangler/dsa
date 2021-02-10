01e42d9441f115bbf555c8b764e82a590a6bcf5c
NickSpangler <Spangleyman@mac.com>
2021-02-10 16:01:38 -0500

    define instance method

--- ES2015/classes.js
+++ ES2015/classes.js
@@ -2,5 +2,20 @@
 //PRIMARILY SYNTACTICAL SUGAR -> just an easier syntax
 //Implememnt a class for each kind of data structure as a class
 
+//simple example of a class, using class constructor
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
+    //instance method
+    fullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
 
+let nick = new Person("Nick", "Spangler")
+
+//Instance methods
+    //functionality that pertains to a specific INSTANCE of a class
