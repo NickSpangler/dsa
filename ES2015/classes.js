22934c8bfe489730f7e451a3105ae4316ad9f3f7
NickSpangler <Spangleyman@mac.com>
2021-02-10 15:52:10 -0500

    define classes

---
+++ ES2015/classes.js
@@ -0,0 +1,6 @@
+//Classes are blueprints for creating objects with pre-defined properties and methods
+//PRIMARILY SYNTACTICAL SUGAR -> just an easier syntax
+//Implememnt a class for each kind of data structure as a class
+
+
+
