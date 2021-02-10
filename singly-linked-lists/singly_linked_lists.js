7289889b01ba2fb799d5a537700be01dc1e00720
NickSpangler <Spangleyman@mac.com>
2021-02-10 16:20:31 -0500

    define linked list

--- singly-linked-lists/singly_linked_lists.js
+++ singly-linked-lists/singly_linked_lists.js
@@ -1,3 +1,7 @@
 //Define Singly Linked List
 //Compare and contrast with Arrays
 //Implement insertion, removal, and traversal methods on Singly Linked Lists
+
+//Linked list contains a HEAD, TAIL, and LENGTH propery
+
+//Consist of nodes, and eac node has a VALUE and a POINTER to another node, or null
