7246e6684991bfe8f1e71093f33e7cadcffa62be
NickSpangler <Spangleyman@mac.com>
2021-02-10 16:24:04 -0500

    compare lists and arrays

--- singly-linked-lists/singly_linked_lists.js
+++ singly-linked-lists/singly_linked_lists.js
@@ -5,3 +5,15 @@
 //Linked list contains a HEAD, TAIL, and LENGTH propery
 
 //Consist of nodes, and eac node has a VALUE and a POINTER to another node, or null
+
+//COMPARISONS WITH ARRAYS
+    //LISTS
+    //do not have indexes!
+    //connected via nodes with a NEXT pointer
+    //random access is NOT allowed
+
+    //ARRAYS
+    //indexed in order
+    //insertion and deletion can be expensive
+    //can quickly be accessed at a specific index
+
