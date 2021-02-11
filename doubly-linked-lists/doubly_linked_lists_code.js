7c103b960965eb9c181a336ea27c3a02b6d00b33
NickSpangler <Spangleyman@mac.com>
2021-02-11 17:09:13 -0500

    implement unshift()

--- doubly-linked-lists/doubly_linked_lists_code.js
+++ doubly-linked-lists/doubly_linked_lists_code.js
@@ -63,6 +63,20 @@ class doublyLinkedList {
         this.length--
         return node
     }
+
+    unshift(val) {
+        let node = new Node(val)
+        if (!this.head) {
+            this.head = node
+            this.tail = node
+        } else {
+            this.head.prev = node
+            node.next = this.head
+            this.head = node
+        }
+        this.length++
+        return this
+    }
 }
 
 let list = new doublyLinkedList()
