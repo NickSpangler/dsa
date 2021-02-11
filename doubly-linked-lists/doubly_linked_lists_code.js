c00e512c0e7f01ef1112d95f00cd786d1ff559d7
NickSpangler <Spangleyman@mac.com>
2021-02-11 16:29:58 -0500

    implement pop() method

--- doubly-linked-lists/doubly_linked_lists_code.js
+++ doubly-linked-lists/doubly_linked_lists_code.js
@@ -5,7 +5,7 @@ class Node {
     constructor(val) {
         this.val = val
         this.next = null
-        this.previous = null //only difference in node class from SinglyLinkedList
+        this.prev = null //only difference in node class from SinglyLinkedList
     }
 }
 
@@ -20,15 +20,32 @@ class doublyLinkedList {
     }
 
     push(val) {
-        node = new Node(val)
+        let node = new Node(val)
         if (!this.head) {
             this.head = node
             this.tail = node
         } else {
             this.tail.next = node
-            node.previous = this.tail
+            node.prev = this.tail
             this.tail = node
         }
         this.length++
+        return this;
+    }
+
+    pop() {
+        if (!this.head) return undefined
+        let node = this.tail
+        if(this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.tail.prev = this.tail
+            this.tail.next = null
+        }
+        this.length--
+        return node
     }
 }
+
+let list = new doublyLinkedList()
