c5c948ebc4790c142d67d86500ce895acd0e48d0
NickSpangler <Spangleyman@mac.com>
2021-02-11 16:13:25 -0500

    create skeleton of Node class and doublyLinkedList class

---
+++ doubly-linked-lists/doubly_linked_lists_code.js
@@ -0,0 +1,21 @@
+class Node {
+//val
+//next
+//prev
+    constructor(val) {
+        this.val = val
+        this.next = null
+        this.previous = null //only difference in node class from SinglyLinkedList
+    }
+}
+
+class doublyLinkedList {
+//head
+//tail
+//length
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+}
