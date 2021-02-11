47484f6581dcb9d7f7e3e8b5593b2816d2a47490
NickSpangler <Spangleyman@mac.com>
2021-02-11 16:09:10 -0500

    add pros and cons

---
+++ doubly-linked-lists/doubly_linked_lists_notes.js
@@ -0,0 +1,9 @@
+//almost identical to a singly linked list
+//however, each node has a pointer to previous, as well as to next
+
+//COMPARISON WITH SINGLY LINKED LIST
+    //CON -> USES MORE MEMORY!
+    //PRO - MORE FLEXIBILITY
+    //ALMOST ALWAYS A TRADEOFF
+
+    
