11152128ed9f244959eac98f6afa9329fe22c5ae
NickSpangler <Spangleyman@mac.com>
2021-02-12 14:04:19 -0500

    example with an array

--- stacks-and-queues/stacks_and_queues_notes.js
+++ stacks-and-queues/stacks_and_queues_notes.js
@@ -9,4 +9,20 @@
     //Routing -> history object is treated like a stack
 
 //IMPLEMENT OPERATIONS ON A STACK DATA STRUCTURE
+//JAVASCRIPT DOES NOT HAVE ITS OWN STACK DATA STRUCTURE\
+
+//Simply using an Array
+let stack = []
+
+stack.push('Google')
+stack.push('Instagram')
+stack.push('Youtube')
+
+//if you are ONLY pushing and popping to retrieve the data, it is operating as a STACK!
+
+stack.pop()
+stack.pop()
+stack.pop()
+
+//ONLY DO IT THIS WAY. DO NOT SHIFT AND UNSHIFT!!!
 
