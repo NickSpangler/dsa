d21a80583ee3fadb46b0865474854622fd420f6f
NickSpangler <Spangleyman@mac.com>
2021-02-12 14:00:39 -0500

    define stack

--- stacks-and-queues/stacks_and_queues_notes.js
+++ stacks-and-queues/stacks_and_queues_notes.js
@@ -1,5 +1,12 @@
 //WHAT IS A STACK?
+    //LIFO data strcture
+    //LAST IN, FIRST OUT
+
 
 //USE CASES FOR A STACK
+    //Managing function invocations
+    //Undo / Redo
+    //Routing -> history object is treated like a stack
 
 //IMPLEMENT OPERATIONS ON A STACK DATA STRUCTURE
+
