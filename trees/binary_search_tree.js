7c5bebba7f11f43ac67c2a64e6b9c1d5d345cce6
NickSpangler <Spangleyman@mac.com>
2021-02-15 12:31:31 -0500

    implement find()

--- trees/binary_search_tree.js
+++ trees/binary_search_tree.js
@@ -18,36 +18,77 @@ class BST {
             //if there is, compare value
             //if greater, check to see if right node exists, if there is, move there and and repeat, if not, this node is set as the right property
             //if it is less, check to see if left node exists, if yes, move there and repeat, if not, add this node as left property
+    // insert(val) {
+    //     let newNode = new Node(val)
+    //     if (!this.root) {
+    //         this.root = newNode
+    //         return this;
+    //     }
+    //     let current = this.root
+    //     while (true) {
+    //         if (val === current.val) return undefined
+    //         if (newNode.val > current.val) {
+    //             if (!current.right) {
+    //                 current.right = newNode
+    //                 return this
+    //             } else {
+    //                 current = current.right
+    //             }
+    //         } else if (newNode.val < current.val) {
+    //             if (!current.left) {
+    //                 current.left = newNode
+    //                 return this
+    //             } else {
+    //                 current = current.left
+    //             } 
+    //         }
+    //     }
+    // }
+
     insert(val) {
         let newNode = new Node(val)
         if (!this.root) {
-            this.root = newNode
+            this.root = newNode;
             return this;
         }
         let current = this.root
         while (true) {
-            if (newNode.val > current.val) {
-                if (!current.right) {
-                    current.right = newNode
-                    return this
-                } else {
-                    current = current.right
-                }
-            } else {
+            if (val === current.val) return undefined
+            if (val < current.val) {
                 if (!current.left) {
-                    current.left = newNode
-                    return this
-                } else {
-                    current = current.left
-                } 
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if (!current.right) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right
             }
         }
-        return this
+    }
+
+    //FIND
+
+    find(val) {
+        if (!this.root) return false;
+        let current = this.root
+        while(true) {
+            if (current.val === val) return true;
+            if (val < current.val) {
+                if (!current.left) return false
+                else current = current.left
+            } else {
+                if (!current.rigth) return false;
+                else current = current.right
+            }
+        }
     }
 }
 
 let tree = new BST()
-tree.root = new Node(10);
-tree.root.right = new Node(15)
-tree.root.left = new Node(7)
-tree.root.left.right = new Node(9)
+tree.insert(10)
+tree.insert(2)
+tree.insert(13)
