aeda72efae1e3b7337f9af74ab5f0110d41d19c5
NickSpangler <Spangleyman@mac.com>
2021-02-15 14:00:58 -0500

    iterative BFS steps

---
+++ trees/breadth_first_search.js
@@ -0,0 +1,9 @@
+//BFS ITERATIVE STEPS
+
+//create a queue (can just be an array) AND a variable to store values of nodes visited
+    //push the root to the queue
+//loop as long as there is something in the queue
+    //dequeue a node and push the value of the node into the results variable
+    //if there is a left property, push to queue
+    //if there is a right property, push to queue
+//return the variable that stores the values
