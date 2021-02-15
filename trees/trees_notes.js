1e28fdfa273b6e80d4438fb79e84c1fbc33adfcf
NickSpangler <Spangleyman@mac.com>
2021-02-15 10:24:10 -0500

    what is a tree?

--- trees/trees_notes.js
+++ trees/trees_notes.js
@@ -1,7 +1,22 @@
 //TREES
+
 //what is a tree?
+//data structure with NODES, and Parent/Child relationships
+
 
 //compare/contrast with linked lists
+//Lists === LINEAR
+//Trees === NON-LINEAR
+//Technically, singly linked list is a sort of special tree case. (each node happens to only have one child)
+//A node can ONLY point to a CHILD, only nodes BELOW them
+
+//TERMINOLOGY
+//ROOT = The Top node in a tree
+//CHILD = A node directly connected to another node when moving away from the Root
+//PARENT = converse notion of a child
+//SIBLINGS = a group of nodes with the same parent
+//LEAFT = A node with no children
+//EDGE = the connection between one node and another
 
 //Trees / Binary Trees / Binary Search Trees
 
