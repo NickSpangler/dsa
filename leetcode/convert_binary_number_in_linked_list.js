16707e22f7b84a13d830968e909c00384503b5c5
NickSpangler <Spangleyman@mac.com>
2021-02-16 16:04:47 -0500

    traverse linked list

--- leetcode/convert_binary_number_in_linked_list.js
+++ leetcode/convert_binary_number_in_linked_list.js
@@ -1,3 +1,15 @@
 // Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
 
 // Return the decimal value of the number in the linked list.
+
+var getDecimalValue = function(head) {
+    let num = []
+    let current = head
+    while (current) {
+        num.push(current.val)
+        current = current.next
+    }
+    console.log(num.join(''))
+};
+
+console.log(parseInt(101, 2))
