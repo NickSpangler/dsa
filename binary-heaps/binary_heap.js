ff6231300fc895824ab238a91afbb51b05a88952
NickSpangler <Spangleyman@mac.com>
2021-02-17 12:45:48 -0500

    add notes on extractMax() implementation

--- binary-heaps/binary_heap.js
+++ binary-heaps/binary_heap.js
@@ -19,12 +19,27 @@ class MaxBinaryHeap {
         console.log(this.values)
     }
 
+
+    //save first element (root) to return at end
+    //pop last element and make it the value of the first element
+    //make sure the array/heap was larger than one element
+
+    //sink down logic
+    //start at first index, set left and right INDEXES first
+    //define SWAP as the index you might be swapping with, initialized as null
+    //do all comparisons with left first, making sure it exists, seeing if it's larger, and setting swap if so
+    //do comparisons with right, making sure it exists
+        //DOUBLE IF to check both conditionals against SWAP variable
+    //swap is the break, if it remains null
+    //if not, make the swap, and set INDEX to the value of SWAP
+    //return the extracted value
     extractMax() {
         let extracted = this.values[0]
         let end = this.values.pop()
-        this.values[0] = end
-        this.sinkDown()
-        console.log(this.values)
+        if (this.values.length > 0) {
+            this.values[0] = end
+            this.sinkDown()
+        }
         return extracted
     }
 
@@ -35,6 +50,23 @@ class MaxBinaryHeap {
         while(true) {
             let leftIndex = 2*index+1
             let rightIndex = 2*index+2
+            let leftChild, rightChild;
+            let swap = null;
+            if (leftIndex < length) {
+                leftChild = this.values[leftIndex]
+                if (leftChild > element) {
+                    swap = leftIndex
+                }
+            }
+            if (rightIndex < length) {
+                rightChild = this.values[rightIndex]
+                if ((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)) {
+                    swap = rightIndex
+                }
+            }
+            if (swap === null) break
+            [this.values[index], this.values[swap]] = [this.values[swap], this.values[index]]
+            index = swap
         }
     }
 }
