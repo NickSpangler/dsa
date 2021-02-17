939aa3b68433296cc29d70eb5aa3d91963bc1e6d
NickSpangler <Spangleyman@mac.com>
2021-02-17 13:31:47 -0500

    define priority queue

--- binary-heaps/priority_queue.js
+++ binary-heaps/priority_queue.js
@@ -1,3 +1,22 @@
 //What is a priority queue
     //A data structure where each element has a priority
     //Elements with higher priorities are served before elements with lower priorities
+
+//SEPARATE FROM HEAPS!! THEY ARE AN ABSTRACT CONCEPT
+
+//priority queue pseudocode:
+//THIS WILL BE DONE AS A MINBINARYHEAP
+
+class Node {
+    constructor(val, priority) {
+        this.val = val
+        this.priority = priority
+    }
+}
+class PriorityQueue {
+    constructor() {
+        this.values = []
+    }
+
+    
+}
