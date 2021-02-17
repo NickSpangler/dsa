f065fb5c9527b9415dfc9fd0b5ffcae94ab71ad2
NickSpangler <Spangleyman@mac.com>
2021-02-17 11:21:50 -0500

    set objectives

---
+++ binary-heaps/binary_heaps_notes.js
@@ -0,0 +1,9 @@
+//Define Binary Heap
+
+//Compare and Contrast min and max heaps
+
+//implement methods on heaps
+
+//Where are heaps useful in the real world? And what other data
+//structures can be constructed from heaps?
+
