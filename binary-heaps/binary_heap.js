e806a565cd635492ba614029e3b8b4f451f79fe0
NickSpangler <Spangleyman@mac.com>
2021-02-17 12:18:30 -0500

    try to implement extractMax()

--- binary-heaps/binary_heap.js
+++ binary-heaps/binary_heap.js
@@ -18,6 +18,36 @@ class MaxBinaryHeap {
         }
         console.log(this.values)
     }
+
+    extractMax() {
+        let extracted = this.values.shift()
+        this.values.unshift(this.values.pop())
+        let current = 0
+
+            while (current < this.values.length) {
+                let left = this.values[current*2+1] ? current*2+1 : null
+                let right = this.values[current*2+2] ? current*2+2 : null
+                console.log(this.values[current])
+                console.log(this.values[left])
+                console.log(this.values[right])
+                if (this.values[left] > this.values[current] && this.values[right] > this.values[current]) {
+                    let maxIndex = this.values[left] > this.values[right] ? left : right
+                    [this.values[current], this.values[maxIndex]] = [this.values[maxIndex], this.values[current]]
+                    current = maxIndex
+                    console.log(this.values)
+                } else if (this.values[left] > this.values[current] || this.values[right] > this.values[current]) {
+                    let maxIndex = this.values[left] > this.values[right] ? left : right
+                    [this.values[current], this.values[maxIndex]] = [this.values[maxIndex], this.values[current]]
+                    current = maxIndex
+                } else {
+                    break
+                }
+            }
+        
+        console.log(this.values)
+        console.log(extracted)
+        return extracted
+    }
 }
 
 let heap = new MaxBinaryHeap
@@ -31,3 +61,4 @@ heap.insert(74)
 heap.insert(100)
 heap.insert(2)
 heap.insert(400)
+heap.extractMax()
