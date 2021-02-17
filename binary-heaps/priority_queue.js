777258c33083f240853370083179c5aaebfbc6b5
NickSpangler <Spangleyman@mac.com>
2021-02-17 13:55:49 -0500

    attempt dequeue()

--- binary-heaps/priority_queue.js
+++ binary-heaps/priority_queue.js
@@ -37,7 +37,41 @@ class PriorityQueue {
     }
 
     dequeue() {
-        
+        let node = this.values[0]
+        let end = this.values.pop()
+        if (this.values.length > 0) {
+            this.values[0] = end
+            this.sinkDown()
+        }
+        console.log(node)
+        console.log(this.values)
+        return node
+    }
+
+    sinkDown() {
+        let index = 0
+        const length = this.values.length
+        const element = this.values[0]
+        while(true) {
+            let leftIndex = index*2 +1
+            let rightIndex = index*2 +2
+            let leftChild, rightChild, swap = null
+            if (leftIndex < length) {
+                leftChild = this.values[leftIndex]
+                if (element.priority < leftChild.priority) {
+                    swap = leftIndex
+                }
+            }
+            if (rightIndex < length) {
+                rightChild = this.values[rightIndex]
+                if ((swap === null && element.priority < rightChild.priority) || (swap !== null && rightChild.priority < leftChild.priority)) {
+                    swap = rightIndex
+                }
+            }
+            if (swap === null) break
+            [this.values[index], this.values[swap]] = [this.values[swap], this.values[index]]
+            index = swap
+        }
     }
 
 }
@@ -47,3 +81,4 @@ q.enqueue('boo boo', 4)
 q.enqueue('needs stitches', 2)
 q.enqueue('faking it', 5)
 q.enqueue('gunshot wound', 0)
+q.dequeue()
