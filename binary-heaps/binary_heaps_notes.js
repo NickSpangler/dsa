c42c6b55fa0347b2ea1e2451ec299e46f6415041
NickSpangler <Spangleyman@mac.com>
2021-02-17 11:30:50 -0500

    notes an arrays as Binary Heaps

--- binary-heaps/binary_heaps_notes.js
+++ binary-heaps/binary_heaps_notes.js
@@ -3,6 +3,10 @@
     //MaxBinaryHeap - parent nodes are always larger than child nodes
     //MinBinaryHeap - parent nodes are always smaller than child nodes
 
+    //each parent can have at most two child nodes
+    //binary heap is AS COMPACT AS POSSIBLE
+    //All children of each node are as full as they can be, left children filled first
+
 //Compare and Contrast min and max heaps
 
 //implement methods on heaps
@@ -10,3 +14,7 @@
 //Where are heaps useful in the real world? And what other data
 //structures can be constructed from heaps?
 
+//REPRESENTING A BINARY HEAP AS AN ARRAY:
+    //For any index of an array n...
+    //LEFT child is stored at 2n + 1
+    //RIGHT child is stored at 2n + 2
