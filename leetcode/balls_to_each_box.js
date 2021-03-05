eda149ce79fea5bb9a1c37626a39264210599bd9
NickSpangler <Spangleyman@mac.com>
2021-03-05 11:34:48 -0500

    plan with pseudocode

--- leetcode/balls_to_each_box.js
+++ leetcode/balls_to_each_box.js
@@ -6,3 +6,10 @@
 // Return an array answer of size n, where answer[i] is the minimum number of operations needed to move all the balls to the ith box.
 
 // Each answer[i] is calculated considering the initial state of the boxes.
+
+var minOperations = function(boxes) {
+    
+};
+
+//for O(n) time, iterate over the array once, incrementing the result array.
+    //this means for each position [i], calculating the moves FROM that box to every other box, and incrementing those indicies of the result array
