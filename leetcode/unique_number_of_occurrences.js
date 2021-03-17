83fec1e83a94baa229db20bf7dd2553c55abf2af
NickSpangler <Spangleyman@mac.com>
2021-03-17 10:45:29 -0400

    define problem

---
+++ leetcode/unique_number_of_occurrences.js
@@ -0,0 +1,9 @@
+// Given an array of integers arr, 
+// write a function that returns true if and only if the number 
+// of occurrences of each value in the array is unique.
+
+var uniqueOccurrences = function(arr) {
+    
+};
+
+uniqueOccurrences([1,2,2,1,1,3])
