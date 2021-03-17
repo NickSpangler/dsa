c7d408776392e03bf2352f9fd99e1d21541e4b69
NickSpangler <Spangleyman@mac.com>
2021-03-17 11:31:10 -0400

    final

--- leetcode/peak_index_in_mountain_array.js
+++ leetcode/peak_index_in_mountain_array.js
@@ -10,8 +10,15 @@ console.log('up here!')
 // return any i such that arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
 
 var peakIndexInMountainArray = function(arr) {
-    
+    for (let i = 1; i < arr.length - 1; i++) if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) console.log(i)
 };
+
+peakIndexInMountainArray([0,1,0])
+peakIndexInMountainArray([0,2,1,0])
+peakIndexInMountainArray([0,10,5,2])
+peakIndexInMountainArray([3,4,5,1])
+peakIndexInMountainArray([24,69,100,99,79,78,67,36,26,19])
+
 
 // iterate, as soon as i is greater than i - 1 and less than i + 1, return i
 
