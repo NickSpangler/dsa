c7dfb9e917b279410cd34698fd54bb7fba65ccdb
NickSpangler <Spangleyman@mac.com>
2021-03-17 10:58:43 -0400

    cleaned up code, three lines

--- leetcode/unique_number_of_occurrences.js
+++ leetcode/unique_number_of_occurrences.js
@@ -2,18 +2,27 @@
 // write a function that returns true if and only if the number 
 // of occurrences of each value in the array is unique.
 
+console.log('heloo')
+//CLEAN, NO CONSOLES
 var uniqueOccurrences = function(arr) {
     let map = {}
-    for (val of arr) {
-        map[val] ? map[val]++ : map[val] = 1
-    }
-
-    console.log(Object.values(map))
-    console.log(new Set(Object.values(map)))
-
-    console.log(Object.values(map).length === new Set(Object.values(map)).size ? true : false)
+    for (val of arr) map[val] ? map[val]++ : map[val] = 1
     return Object.values(map).length === new Set(Object.values(map)).size ? true : false
 };
+
+
+// var uniqueOccurrences = function(arr) {
+//     let map = {}
+//     for (val of arr) {
+//         map[val] ? map[val]++ : map[val] = 1
+//     }
+
+//     console.log(Object.values(map))
+//     console.log(new Set(Object.values(map)))
+
+//     console.log(Object.values(map).length === new Set(Object.values(map)).size ? true : false)
+//     return Object.values(map).length === new Set(Object.values(map)).size ? true : false
+// };
 
 uniqueOccurrences([1,2,2,1,1,3])
 
