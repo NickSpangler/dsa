00b1e11f0a5b9291688636cf7c2f23810e5347b5
NickSpangler <Spangleyman@mac.com>
2021-03-22 15:42:47 -0400

    pseudocode answer

--- leetcode/middle_of_the_linked_list.js
+++ leetcode/middle_of_the_linked_list.js
@@ -4,3 +4,11 @@ console.log('middle')
 
 // If there are two middle nodes, return the second middle node.
 
+var middleNode = function(head) {
+    
+}; 
+
+// find length of list
+// if odd, find Math.floor(list / 2) + 1
+// if even, find list / 2 + 1
+// return that node using counter?
