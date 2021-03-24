45ec0b3d93b1b92e4644b2de77d56eaf65fc7501
NickSpangler <Spangleyman@mac.com>
2021-03-24 14:31:27 -0400

    isolate non-numbers

--- leetcode/letter_case_permutation.js
+++ leetcode/letter_case_permutation.js
@@ -4,6 +4,35 @@ console.log('gotta find em all')
 
 // Return a list of all possible strings we could create. You can return the output in any order.
 
+
+
+
 var letterCasePermutation = function(S) {
+    let res = []
+    const upCase = (index) => {
+        
+    }
     
+    const downCase = (index) => {
+        
+    }
+
+    for (let i = 0; i < S.length; i++) {
+        //this isolates non-digits -> only applies to letters
+        if (!(S.charCodeAt(i) >= 48 && S.charCodeAt(i) <= 57)) {
+            console.log(S[i], S.charCodeAt(i))
+        }
+    }
 };
+
+letterCasePermutation("a1b2")
+
+// using recursion?
+
+// iterate through
+
+// push string with current character capital and lowercase
+
+// then keep going recursively
+
+
