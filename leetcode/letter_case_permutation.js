6834557fdb34eba1b98464338dfe4844f5871426
NickSpangler <Spangleyman@mac.com>
2021-03-24 14:08:45 -0400

    state problem

---
+++ leetcode/letter_case_permutation.js
@@ -0,0 +1,9 @@
+console.log('gotta find em all')
+
+// Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.
+
+// Return a list of all possible strings we could create. You can return the output in any order.
+
+var letterCasePermutation = function(S) {
+    
+};
