7053e52a16ae449e84b554c733fb05317a2cd2d7
NickSpangler <Spangleyman@mac.com>
2021-03-25 16:51:09 -0400

    create rows / columns maps

--- leetcode/queens_can_attack.js
+++ leetcode/queens_can_attack.js
@@ -7,6 +7,19 @@ console.log('attack!')
 // return the coordinates of all the queens (in any order) that can attack the King.
 
 var queensAttacktheKing = function(queens, king) {
+    let kingRow = king[0], kingColumn = king[1]
+    let rows = {}, columns = {}
+
+    for (queen of queens) {
+        let queenRow = queen[0], queenColumn = queen[1]
+
+        if (queenRow === 5) console.log('WTF its a 5')
+        rows[queenRow] ? rows[queenRow].push(queenColumn) : rows[queenRow] = [queenColumn]
+        columns[queenColumn] ? columns[queenColumn].push(queenRow) : columns[queenColumn] = [queenRow]
+    }
+    console.log(rows)
+    console.log(columns)
+
     
 };
 
@@ -17,3 +30,5 @@ var queensAttacktheKing = function(queens, king) {
 // use columns as keys, with an array of rows as the values
 
 // search in all four directions, starting from King and moving outward
+
+queensAttacktheKing(queens = [[0,0],[1,1],[2,2],[3,4],[3,5],[4,4],[4,5]], king = [3,3])
